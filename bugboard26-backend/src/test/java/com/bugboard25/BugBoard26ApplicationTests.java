6c045787fadd3f5b94befb15ece2fd6c6ca7c0b3
alewildb <wildbandana.ale@gmail.com>
2026-03-05 15:00:00 +0100

    test(backend): add unit tests for application context and project controller

---
+++ bugboard26-backend/src/test/java/com/bugboard25/BugBoard26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugboard25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBoard26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
